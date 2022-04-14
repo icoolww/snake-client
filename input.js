461f823b23d97930f296e2391014ac945c7822c6
William <williamwijaya.mail@gmail.com>
2022-04-14 05:42:08 +0000

    Snek

--- input.js
+++ input.js
@@ -8,50 +8,53 @@ let connection;
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
-  return stdin;
-};
 
 
-const handleUserInput = function (key) {
-  
-  if (key === "w") {
-    conn.write("Move: up")
-  } else if (key === "s") {
-    conn.write("Move: down")
-  } else if (key === "a") {
-    conn.write("Move: left")
-  } else if (key === "d") {
-    conn.write("Move: right")
-  } else if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-
-const specialKey = function (key) {
-  
-  if (key === "c") {
-    conn.write("Say: cool and awesome !!")
-  } else if (key === "t") {
-    conn.write("Say: there you go !!")
-  } else if (key === "o") {
-    conn.write("Say: oouch !!")
+  const handleUserInput = function (key) {
+    
+    if (key === "w") {
+      conn.write("Move: up")
+    } else if (key === "s") {
+      conn.write("Move: down")
+    } else if (key === "a") {
+      conn.write("Move: left")
+    } else if (key === "d") {
+      conn.write("Move: right")
+    } else if (key === '\u0003') {
+      process.exit();
+    }
+  }; 
+    
+    const specialKey = function (key) {
+      if (key === "c") {
+        conn.write("Say: cool and awesome !!")
+      } else if (key === "t") {
+        conn.write("Say: there you go !!")
+      } else if (key === "o") {
+        conn.write("Say: oouch !!")
+      }
+    };
+      
+      stdin.on("data", handleUserInput);
+      stdin.on("data", specialKey);
+      
+      
+    return stdin;
+    
   };
-
-
-console.log("Connecting ...");
-connect();
-setupInput();
-handleUserInput();
-specialKey();
-
-module.exports = {
-  setupInput
-};
-
+  
+  
+  console.log("Connecting ...");
+  const conn = connect();
+  setupInput(conn);
+  // handleUserInput();
+  // specialKey();
+  
+  module.exports = {
+    setupInput
+  };
+  
+  
